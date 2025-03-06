a908aeb5cb6a73968d8c282cecdc30eef04c29a6
Thomas Vergne <contact@thomas-vergne.fr>
2025-03-06 13:52:46 +0100

    build(astro): fixed base name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thomasvergne.github.io',
-  base: 'donaldcss',
+  base: 'donald.css',
   vite: {
     plugins: [tailwindcss()],
   },
